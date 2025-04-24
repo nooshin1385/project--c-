c33b20ce0bed28858206e0c9b78754c5d1023951
nooshin <nooshinmishmast3@gmail.com>
2025-04-24 17:18:13 +0330

    "2 set functions are addad"

--- phase1.cpp
+++ phase1.cpp
@@ -1,2 +1,66 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
+
+class Student
+{
+    int UserId;
+    string StudentId;
+    string Name;
+    string Email;
+    float Balance;
+    bool IsActive;
+
+public:
+    Student(int user_id, string student_id, string _name, string _email, float _balance, bool is_active = true)
+    {
+        try
+        {
+            setUserId(to_string(user_id));
+        }
+        catch (const char *msg)
+        {
+            cout << msg << endl;
+        }
+
+        StudentId = student_id;
+        Name = _name;
+        Email = _email;
+        Balance = _balance;
+        IsActive = is_active;
+    }
+
+    void setUserId(string _userID)
+    {
+        int count = 0;
+        for (char c : _userID)
+        {
+            if (!isdigit(c))
+            {
+                throw "UserID must be just digits.";
+            }
+            count++;
+        }
+
+        if (count != 10)
+        {
+            throw "UserID must be exactly 10 digits.";
+        }
+
+        UserId = stoi(_userID);
+    }
+    void setstudentid(string _studentID)
+    {
+        int count = 0;
+        for (char c : _studentID)
+        {
+            if (!isdigit(c))
+            {
+                throw "studentID must be just digits.";
+            }
+            count++;
+        }
+        if (count != 10)
+            throw "studentID must be exactly 10 digits.";
+        StudentId = _studentID;
+    }
+};
