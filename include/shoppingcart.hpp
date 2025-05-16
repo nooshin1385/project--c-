d77e84e74a08070e22e960acf1ce464b117dffeb
nooshin <nooshinmishmast3@gmail.com>
2025-05-16 16:18:18 +0330

    ایجاد کلاس shoppingcart

---
+++ include/shoppingcart.hpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include "reservation.hpp"
+using namespace std;
+class ShoppingCart
+{
+    vector<Reservation> reservation;
+
+public:
+    // Transaction confirm();
+    void addReservation(Reservation const &r)
+    {
+        reservation.push_back(r);
+        cout << "reserve added to cart ." << endl;
+    }
+    void removeReservation(int ID)
+    {
+        for (size_t i = 0; i < reservation.size(); i++)
+        {
+            if (reservation[i].getReservationId() == ID)
+            {
+            }
+        }
+    }
+    void viewShoppingCartItems()
+    {
+        if (reservation.empty())
+        {
+            cout << "shopping cart is empty. \n";
+            return;
+        }
+        else
+        {
+            cout << "----Shopping Cart Itemes: ---\n";
+            for (Reservation &r : reservation)
+            {
+                r.printReservationinfo();
+            }
+        }
+    }
+    void clear()
+    {
+        reservation.clear();
+        cout << "Shopping cart si clear now.\n";
+    }
+    const vector <Reservation>&getreservation() const {
+        return reservation ;
+    }
+};
