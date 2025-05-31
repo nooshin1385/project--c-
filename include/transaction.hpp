443f6dc2f2d474ffb6994c4f6e780f182d7bd894
nooshin <nooshinmishmast3@gmail.com>
2025-05-31 20:39:07 +0330

    پیاده سازی توابع set & get  برای کلاس transaaction

--- include/transaction.hpp
+++ include/transaction.hpp
@@ -27,5 +27,16 @@ public:
     {
         CreatedAt = time(nullptr);
     }
-    
+    void setTransactionID(int id) { TransactionID = id; }
+    void setTrackingCode(const string &code) { TrackingCode = code; }
+    void setAmount(float amount) { Amount = amount; }
+    void setType(TransactionType type) { Type = type; }
+    void setStatus(TransactionStatus status) { Status = status; }
+    void setCreatedAt(time_t t) { CreatedAt = t; }
+    int getTransactionID() const { return TransactionID; }
+    string getTrackingCode() const { return TrackingCode; }
+    float getAmount() const { return Amount; }
+    TransactionType getType() const { return Type; }
+    TransactionStatus getStatus() const { return Status; }
+    time_t getCreatedAt() const { return CreatedAt; }
 };
