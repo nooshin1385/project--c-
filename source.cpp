2b020d309d64e0359b4d83c212bec24065e882be
nooshin <nooshinmishmast3@gmail.com>
2025-07-09 18:07:27 +0330

    شروع فاز 4 پروژه ...

---
+++ source.cpp
@@ -0,0 +1,20 @@
+#include "bcrypt/BCrypt.hpp"
+#include "json.hpp"
+#include <iostream>
+using namespace std;
+using json = nlohmann::json;
+
+int main()
+{
+	string password = "test";
+	string hash = BCrypt::generateHash(password);
+	cout << hash;
+	cout << BCrypt::validatePassword(password, hash) << endl;
+	cout << BCrypt::validatePassword("test1", hash) << endl;
+	json j{{"name", "nooshin"},
+		   {"family", "mishmast"}};
+	cout << j["name"] << endl;
+	cout << j["family"];
+
+	return 0;
+}
